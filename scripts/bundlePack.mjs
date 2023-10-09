89d6a1fb6bc46fabe6d089ce21d147d57ffb9b73
SanikaMagar <97955725+SanikaMagar@users.noreply.github.com>
2023-10-10 00:34:11 +0530

    Update bundlePack.mjs
    added addresses

--- scripts/bundlePack.mjs
+++ scripts/bundlePack.mjs
@@ -3,8 +3,8 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 (async () => {
     const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "mumbai");
 
-    const packAddress = " "; // enter the address of pack 
-    const cardAddress = " "; //enter the address of card
+    const packAddress = "0x506e46b45D6C7Dd79BE5bA39661B0a63D585090b "; // enter the address of pack 
+    const cardAddress = " 0xa5239b286BDD5445bA15cb8FB412775f53a7998E"; //enter the address of card
 
     const pack = sdk.getContract(packAddress, "pack");
     const card = sdk.getContract(cardAddress, "edition");
